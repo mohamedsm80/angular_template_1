a8d10254237e63dbdbb14c36229952150beac8e3
mohamedsm80 <mohamed.shawky.m@outlook.com>
2025-12-30 16:54:25 +0200

    update feature

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { MatSidenavContainer } from '@angular/material/sidenav';
+import { MatSidenavContent } from '@angular/material/sidenav';
+import { MatSidenav } from '@angular/material/sidenav';
+
+import { CustomSidenav } from "./components/custom-sidenav/custom-sidenav";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatSidenavContainer, MatSidenavContent, MatSidenav, CustomSidenav],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('demo_1');
+  collapsed = signal(false);
+  sidenavWidth = computed(() => {
+    return this.collapsed() ? '65px' : '250px';
+  })
 }
